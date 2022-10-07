e8d562f86b2c3903f92a8bd9963910adc1c9eb77
Momoka <79566882+obrmmk@users.noreply.github.com>
2022-10-07 15:32:44 +0900

    Update finetune.py

--- examples/seq2seq/finetune.py
+++ examples/seq2seq/finetune.py
@@ -64,7 +64,7 @@ class SummarizationModule(BaseTransformer):
 
         super().__init__(hparams, num_labels=None, mode=self.mode, **kwargs)
         use_task_specific_params(self.model, "summarization")
-        save_git_info(self.hparams.output_dir)
+#         save_git_info(self.hparams.output_dir)
         self.metrics_save_path = Path(self.output_dir) / "metrics.json"
         self.hparams_save_path = Path(self.output_dir) / "hparams.pkl"
         pickle_save(self.hparams, self.hparams_save_path)
